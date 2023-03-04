93426ad3954901e6be05cbf4454cd0cdd857b29f
jqkk <kkj2551@naver.com>
2023-03-05 01:39:54 +0900

    :bug: feat: 불필요한 코드 삭제

--- src/domains/components/QuestionForm.tsx
+++ src/domains/components/QuestionForm.tsx
@@ -1,55 +1,33 @@
 import React from 'react';
 
-import { useAtomValue, useSetAtom } from 'jotai';
 import tw from 'twin.macro';
 
 import { Button, Input } from '@/components';
 import { useInput } from '@/hooks';
 
-import QuestionRegisterPopup from './QuestionRegisterPopup';
-import TermOfUsePopup from './TermOfUsePopup';
-import {
-  dequeueModalPopupQueueAtom,
-  pushModalPopupQueueAtom,
-} from '../store/actions';
-import { currentModalPopupAtom } from '../store/atoms';
 import { 이용약관동의여부확인 } from '../utils';
 
 const QuestionForm = () => {
-  const modalPopup = useAtomValue(currentModalPopupAtom);
-  const createModalPopup = useSetAtom(pushModalPopupQueueAtom);
-  const closeModalPopup = useSetAtom(dequeueModalPopupQueueAtom);
-  const { value: question, onChange, reset } = useInput();
+  const { value: question, onChange } = useInput();
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     const 약관동의여부 = 이용약관동의여부확인();
     if (!약관동의여부) {
-      createModalPopup(<TermOfUsePopup />);
+      //
     }
-    createModalPopup(
-      <QuestionRegisterPopup
-        onConfirm={() => {
-          closeModalPopup();
-          reset();
-        }}
-      />,
-    );
   };
 
   return (
-    <>
-      <Form onSubmit={handleSubmit}>
-        <Input
-          placeholder='어떤 내용이 궁금한가요?'
-          tw='mb-7'
-          onChange={onChange}
-          value={question}
-        />
-        <Button type='submit'>질문하기</Button>
-      </Form>
-      {modalPopup}
-    </>
+    <Form onSubmit={handleSubmit}>
+      <Input
+        placeholder='어떤 내용이 궁금한가요?'
+        tw='mb-7'
+        onChange={onChange}
+        value={question}
+      />
+      <Button type='submit'>질문하기</Button>
+    </Form>
   );
 };
 
--- src/domains/store/atoms/modalPopupQueue.ts
+++
@@ -1,8 +0,0 @@
-import { atom } from 'jotai';
-import { atomWithReset } from 'jotai/utils';
-
-export const modalPopupQueueAtom = atomWithReset<JSX.Element[]>([]);
-
-export const currentModalPopupAtom = atom(
-  (get) => get(modalPopupQueueAtom)[0] ?? null,
-);
