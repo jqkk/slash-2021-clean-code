4fb5335860457cc2894c8b109f1882d4b65334b5
jqkk <kkj2551@naver.com>
2023-03-07 00:46:25 +0900

    :sparkles: feat: alert 선언적으로 구현하기

--- src/domains/components/QuestionForm.tsx
+++ src/domains/components/QuestionForm.tsx
@@ -2,20 +2,46 @@ import React from 'react';
 
 import tw from 'twin.macro';
 
-import { Button, Input } from '@/components';
-import { useInput } from '@/hooks';
+import {
+  BasicAlert,
+  Button,
+  ConfirmAlert,
+  DetailAlert,
+  Input,
+} from '@/components';
+import { useInput, useAlert } from '@/hooks';
 
-import { 이용약관동의여부확인 } from '../utils';
+import { 이용약관동의여부확인, 이용약관동의하기 } from '../utils';
 
 const QuestionForm = () => {
   const { value: question, onChange } = useInput();
+  const { alert } = useAlert();
 
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     const 약관동의여부 = 이용약관동의여부확인();
     if (!약관동의여부) {
-      //
+      const response = await alert(ConfirmAlert, {
+        titleText: '약관에 동의하시겠습니까?',
+        infoText: '약관 동의가 필요합니다.',
+        onConfirm: () => {
+          이용약관동의하기();
+        },
+      });
+      if (!response) {
+        return;
+      }
     }
+
+    await alert(DetailAlert, {
+      titleText: '질문하기',
+      infoText: 'KIM이 설명해드려요',
+    });
+
+    alert(BasicAlert, {
+      titleText: '질문이 등록되었어요',
+      infoText: '답변이 등록되면 알려드릴게요',
+    });
   };
 
   return (
