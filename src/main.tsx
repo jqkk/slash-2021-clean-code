e4ae2043351dcbd88bd496911bc5f525f22c2c0a
jqkk <kkj2551@naver.com>
2023-03-12 21:23:19 +0900

    :truck: chore: react-query 삭제

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import { Provider } from 'jotai';
 import { DevTools } from 'jotai-devtools';
-import { queryClientAtom } from 'jotai-tanstack-query';
 
 import { worker } from '@/mocks/worker';
 import GlobalStyles from '@/styles/GlobalStyles';
@@ -16,19 +12,10 @@ worker.start({
   onUnhandledRequest: 'bypass',
 });
 
-const queryClient = new QueryClient();
-
-queryClientAtom.init = queryClient;
-
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <Provider>
-        <GlobalStyles />
-        <App />
-        <DevTools />
-      </Provider>
-      <ReactQueryDevtools position='bottom-right' />
-    </QueryClientProvider>
+    <GlobalStyles />
+    <App />
+    <DevTools />
   </React.StrictMode>,
 );
