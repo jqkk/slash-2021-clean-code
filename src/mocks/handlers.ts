383b30f6c66fc384d69e18b86a44ec71e12d6788
jqkk <kkj2551@naver.com>
2023-03-12 17:35:31 +0900

    :sparkles: feat: api 작성

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,1 +1,25 @@
-export const handlers = [];
+import { rest } from 'msw';
+
+const generateRandomBoolean = () => Math.random() < 0.5;
+
+let agreement = false;
+
+export const handlers = [
+  rest.get('/api/agreement', (req, res, ctx) => res(ctx.json({ agreement }))),
+  rest.post('/api/agreement', (req, res, ctx) => {
+    agreement = true;
+    return res(ctx.status(201));
+  }),
+  rest.get('/api/counselor', (req, res, ctx) => {
+    const isWaitingForCounselor = generateRandomBoolean();
+    if (isWaitingForCounselor) {
+      return res(
+        ctx.json({
+          name: 'John Doe',
+          image: '',
+        }),
+      );
+    }
+    return res(ctx.json(null));
+  }),
+];
