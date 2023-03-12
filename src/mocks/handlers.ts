9fe2113edce578a275a3fe55adbfa8b0be9dfb6b
jqkk <kkj2551@naver.com>
2023-03-12 23:15:03 +0900

    :sparkles: feat: mock sever api 수정

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -2,21 +2,33 @@ import { rest } from 'msw';
 
 const generateRandomBoolean = () => Math.random() < 0.5;
 
+const delay = (ms: number) =>
+  new Promise<void>((resolve) => {
+    setTimeout(() => {
+      resolve();
+    }, ms);
+  });
+
 let agreement = false;
 
 export const handlers = [
-  rest.get('/api/agreement', (req, res, ctx) => res(ctx.json({ agreement }))),
-  rest.post('/api/agreement', (req, res, ctx) => {
+  rest.get('/api/agreement', async (req, res, ctx) => {
+    await delay(1000);
+    return res(ctx.json({ agreement }));
+  }),
+  rest.post('/api/agreement', async (req, res, ctx) => {
+    await delay(1000);
     agreement = true;
     return res(ctx.status(201));
   }),
-  rest.get('/api/counselor', (req, res, ctx) => {
+  rest.get('/api/counselor', async (req, res, ctx) => {
+    await delay(1000);
     const isWaitingForCounselor = generateRandomBoolean();
     if (isWaitingForCounselor) {
       return res(
         ctx.json({
           name: 'John Doe',
-          image: '',
+          image: '/assets/svgs/Avatar.svg',
         }),
       );
     }
