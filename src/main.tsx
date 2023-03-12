9ee16e39cc6c7a32532a4dab880b78e126f6f091
jqkk <kkj2551@naver.com>
2023-03-12 17:22:23 +0900

    :sparkles: feat: add msw

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,14 @@ import ReactDOM from 'react-dom/client';
 
 import { DevTools } from 'jotai-devtools';
 
+import { worker } from '@/mocks/worker';
 import GlobalStyles from '@/styles/GlobalStyles';
 
 import App from './App';
+
+worker.start({
+  onUnhandledRequest: 'bypass',
+});
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
