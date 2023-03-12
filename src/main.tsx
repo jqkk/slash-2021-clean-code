59819c9710447d37853e13384a925844b86c35f3
jqkk <kkj2551@naver.com>
2023-03-12 21:21:45 +0900

    :sparkles: feat: react-query 적용

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { Provider } from 'jotai';
 import { DevTools } from 'jotai-devtools';
+import { queryClientAtom } from 'jotai-tanstack-query';
 
 import { worker } from '@/mocks/worker';
 import GlobalStyles from '@/styles/GlobalStyles';
@@ -12,10 +16,19 @@ worker.start({
   onUnhandledRequest: 'bypass',
 });
 
+const queryClient = new QueryClient();
+
+queryClientAtom.init = queryClient;
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <GlobalStyles />
-    <App />
-    <DevTools />
+    <QueryClientProvider client={queryClient}>
+      <Provider>
+        <GlobalStyles />
+        <App />
+        <DevTools />
+      </Provider>
+      <ReactQueryDevtools position='bottom-right' />
+    </QueryClientProvider>
   </React.StrictMode>,
 );
