e0fc09ea688bf48832bd3a21e7de0491f74e8656
jqkk <kkj2551@naver.com>
2023-03-23 01:42:28 +0900

    🚚 chore: Alert 폴더 삭제

--- src/components/Alert/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as Alert } from './Alert';
-export { default as BasicAlert } from './BasicAlert';
-export { default as ConfirmAlert } from './ConfirmAlert';
-export { default as DetailAlert } from './DetailAlert';
